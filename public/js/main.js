ae2d3eeafea4bb96390b2639268e07b52bb00356
boceksoft_ofis <huseyin.topuz35@gmail.com>
2024-05-27 10:40:56 +0300

    working with posts

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 const output = document.querySelector("#output");
 const getPostsBtn = document.querySelector("#get-posts-btn");
-
+const postForm = document.querySelector("#post-form");
 // Get and Show Post
 
 async function showPosts() {
@@ -13,12 +13,45 @@ async function showPosts() {
     const posts = await res.json();
     output.innerHTML = "";
     posts.forEach((post) => {
-      output.innerHTML += `<p>${post.body}</p>`;
+      output.innerHTML += `<p>${post.title}</p>`;
     });
   } catch (error) {
     console.log(error);
   }
 }
 
+// submit form
+
+async function addPost(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const title = formData.get("title");
+
+  try {
+    const res = await fetch("http://localhost:8000/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
+    if (!res.ok) {
+      const message = `An error has occured: ${res.status}`;
+      throw new Error(message);
+    }
+    const newPost = await res.json();
+
+    const postEl = document.createElement("div");
+    postEl.textContent = newPost.title;
+    output.appendChild(postEl);
+    showPosts();
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+
 // Event Listeners
 getPostsBtn.addEventListener("click", showPosts);
+postForm.addEventListener("submit", addPost);
